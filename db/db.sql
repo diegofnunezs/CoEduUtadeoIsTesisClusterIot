1bcb54b0833f0e7d189446ad91b75ecef312dcdf
Juan Cantor <juan.cantor@utadeo.edu.co>
2019-05-15 15:01:30 -0500

    Database debug & script db debug

--- db/db.sql
+++ db/db.sql
@@ -4,6 +4,30 @@ DROP TABLE IF EXISTS sensor_metadata;
 DROP TABLE IF EXISTS node_sensor;
 DROP TABLE IF EXISTS network_metadata; 
 
+
+CREATE TABLE network_metadata (
+        sense_interval INTEGER NOT NULL 
+);
+
+CREATE TABLE node_metadata (
+        node_id INTEGER NOT NULL ,
+        location VARCHAR(15) NOT NULL ,
+        PRIMARY KEY(node_id)
+);
+
+CREATE TABLE sensor_metadata (
+        sensor_id INTEGER NOT NULL ,
+        description VARCHAR(15) NOT NULL 
+);
+
+CREATE TABLE node_sensor (
+        node_id INTEGER NOT NULL ,
+        sensor_id INTEGER NOT NULL ,
+        node_pin INTEGER NOT NULL ,
+        data_position INTEGER, 
+        FOREIGN KEY (node_id) REFERENCES node_metadata(node_id)
+);
+
 CREATE TABLE raw_sensor_data (
         node_id INTEGER NOT NULL ,
         time TIME NOT NULL ,
@@ -11,24 +35,3 @@ CREATE TABLE raw_sensor_data (
         value INTEGER NOT NULL ,
         FOREIGN KEY (node_id) REFERENCES node_metadata(node_id)
 );
-CREATE TABLE network_metadata (
-        sense_interval INTEGER NOT NULL 
-);
-CREATE TABLE node_metadata (
-        node_id INTEGER NOT NULL ,
-        location VARCHAR(15) NOT NULL ,
-        PRIMARY KEY(node_id)
-);
-CREATE TABLE sensor_metadata (
-        sensor_id INTEGER NOT NULL ,
-        description VARCHAR(15) NOT NULL 
-);
-CREATE TABLE node_sensor (
-        node_id INTEGER NOT NULL ,
-        sensor_id  NOT NULL ,
-        node_pin INTEGER NOT NULL ,
-        data_position INTEGER, 
-        FOREIGN KEY (node_id) REFERENCES node_metadata(node_id)
-);
-
-
