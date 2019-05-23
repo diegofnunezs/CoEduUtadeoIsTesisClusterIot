40bcbd0ed5ca9c5a422c014837a5e8514d04a8fc
diegoF_95 <diegof.nunezs@utadeo.edu.co>
2019-05-22 19:59:14 -0500

    Update ignore database file

--- db/metadata.sql
+++ db/metadata.sql
@@ -1,18 +1,17 @@
 INSERT INTO network_metadata (sense_time) VALUES (600);
 
-INSERT INTO node_metadata (node_id, location) VALUES (1, 'Study');
 INSERT INTO node_metadata (node_id, location) VALUES (2, 'Living Room');
-INSERT INTO node_metadata (node_id, location) VALUES (3, 'Kitchen');
+
 
 INSERT INTO sensor_metadata (sensor_id, description) VALUES (1, 'Movement');
 INSERT INTO sensor_metadata (sensor_id, description) VALUES (2, 'Temperature');
 INSERT INTO sensor_metadata (sensor_id, description) VALUES (3, 'Humidity');
 INSERT INTO sensor_metadata (sensor_id, description) VALUES (4, 'Light');
 
-INSERT INTO node_sensor (node_id, sensor_id, node_pin, data_position) VALUES (1, 'Movement', 4,null);
+INSERT INTO node_sensor (node_id, sensor_id, node_pin, data_position) VALUES (2, 'Movement', 4,null);
 INSERT INTO node_sensor (node_id, sensor_id, node_pin, data_position) VALUES (2, 'Temperature', 17,1);
-INSERT INTO node_sensor (node_id, sensor_id, node_pin, data_position) VALUES (3, 'Humidity', 17,0);
-INSERT INTO node_sensor (node_id, sensor_id, node_pin, data_position) VALUES (4, 'Light', 14,null);
+INSERT INTO node_sensor (node_id, sensor_id, node_pin, data_position) VALUES (2, 'Humidity', 17,0);
+INSERT INTO node_sensor (node_id, sensor_id, node_pin, data_position) VALUES (2, 'Light', 14,null);
 
 
 
