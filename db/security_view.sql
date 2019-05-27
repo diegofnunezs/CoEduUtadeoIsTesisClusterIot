29cda0a23e7cfe1898ca89010f68ea6d61d717ae
diegoF_95 <diegof.nunezs@utadeo.edu.co>
2019-05-27 17:55:22 -0500

    Create views for Securitu, Light and Movement

---
+++ db/security_view.sql
@@ -0,0 +1,6 @@
+drop view security_view;
+create view security_view as
+select movement_view.Node as Node, movement_view.Time as Time, movement_view.Location as Location, movement_view.Movement as Movement, light_view.Light as Light
+from movement_view, light_view
+where movement_view.Node = movement_view.Node
+group by movement_view.Time
